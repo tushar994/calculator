0cf9ffe6b1a7488ebca32d0b597f8774cfb3b60e
tushar994 <tkumar994@gmail.com>
2020-02-28 04:44:42 +0530

    completed it, only need to design it now

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from config import *
-
+import random
 
 root = tk.Tk()
 root.geometry(str(display_height)+"x"+str(display_width))
@@ -9,38 +9,76 @@ expression = str()
 
 def add_to_str(to_add):
     global expression
-    expression+=str(to_add)
+    expression=expression + to_add
+    print(to_add)
+    text_box.config(text=expression)
 
-text_box=tk.Label(root,text=expression,height=2,width=20)
-text_box.grid(row=0,column=0,columnspan=3,rowspan=text_row_span)
+text_box=tk.Label(root, text=expression ,height=2)
+# text_box.grid(row=0,column=0,columnspan=6,rowspan=text_row_span)
+text_box.pack()
 
-
-
-def create_button1(row,column,text,function,argument):
-    b=tk.Button(root,text=text,fg='blue',bg='black',command=function(argument))
-    b.config(height=2,width=5)
-    b.grid(row=row,column=column)
-
-def create_button(row,column,text):
-    b=tk.Button(root,text=text,fg='blue',bg='black')
-    b.config(height=2,width=5)
-    b.grid(row=row,column=column)
+def find():
+    global expression
 
 
+
+
+
+def get_insult():
+    global expression
+    expression = str()
+    insult_size=len(insults)
+    print(insult_size)
+    expression=insults[random.randint(0,insult_size-1)]
+    print(expression)
+    text_box.config(text=expression)
+    expression=str()
+
+frame1=tk.Frame(root)
+frame1.pack()
+okaygay=0
 for i in range (9):
     w=i%3
     h=int(i/3)
-    create_button1(text_row_span+h,w,i+1,add_to_str,i+1)
+    # create_button1(text_row_span+h,w,i+1,add_to_str,i+1)
+    b = (tk.Button(frame1,text=str(i+1),fg='blue',bg='red',
+    command=lambda gga=str(i+1): add_to_str(gga)))
+    b.config(height=2,width=5)
+    b.grid(row=text_row_span+h,column=(2*w))
 
 
 
-create_button(text_row_span+3,1,"0")
-create_button(text_row_span+3,0,"+")
-create_button(text_row_span+3,2,"-")
+b = (tk.Button(frame1,text="0",fg='blue',bg='red',
+    command=lambda gga=str(0): add_to_str(gga)))
+b.config(height=2,width=5)
+b.grid(row=text_row_span+3,column=2)
 
-create_button(text_row_span+4,0,"*")
-create_button(text_row_span+4,1,"/")
-create_button(text_row_span+4,2,"=")
+
+b = (tk.Button(frame1,text="+",fg='blue',bg='red',
+    command=lambda gga="+": add_to_str(gga)))
+b.config(height=2,width=5)
+b.grid(row=text_row_span+3,column=0)
+
+b = (tk.Button(frame1,text="-",fg='blue',bg='red',
+    command=lambda gga="-": add_to_str(gga)))
+b.config(height=2,width=5)
+b.grid(row=text_row_span+3,column=4)
+
+b = (tk.Button(frame1,text="*",fg='blue',bg='red',
+    command=lambda gga="*": add_to_str(gga)))
+b.config(height=2,width=5)
+b.grid(row=text_row_span+4,column=0)
+
+b = (tk.Button(frame1,text="/",fg='blue',bg='red',
+    command=lambda gga="/": add_to_str(gga)))
+b.config(height=2,width=5)
+b.grid(row=text_row_span+4,column=2)
+
+
+# create_button(text_row_span+4,2,"=",command=lambda: get_insult())
+a=tk.Button(frame1,text="=",fg='blue',bg='black',command=get_insult)
+a.config(height=2,width=5)
+a.grid(row=text_row_span+4,column=4)
 
 
 
@@ -51,5 +89,5 @@ for i in range (3):
     root.grid_rowconfigure(k,minsize=20)
 
 
-print(expression)
+print("it is",expression)
 root.mainloop()
--- config.py
+++ config.py
@@ -2,7 +2,16 @@ display_height = 500
 display_width=500
 bg_color = "#000000"
 
-text_row_span=3
+text_row_span=1
+
+column_start=2
+insults = ["what a retard, cant fucking solve this",
+            "even my grandmother can solve this",
+            "if calculators hadn't been invented,natural selection would have killed you off by now"
+            ,"ask a fucking kindergardener",
+            "i know a foetus that can solve that",
+            "no wonder your parents bailed on you for a chinese kid",
+            "incest, people..."]
 
 # buttonx=[0.1,0.2,0.3]
 # bottony=[0.1,0.2,0.3]
